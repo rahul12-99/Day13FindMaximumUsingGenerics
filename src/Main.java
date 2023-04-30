c513f3ff7b2f0f82da8e0204f9954425ec6ce4c1
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-01 01:17:04 +0530

    Added : Welcome message

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /*
+     * Method to print welcome message
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to generics problem!");
+    }
+}
