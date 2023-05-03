7b262987c639b8622b55a67c03382a91afabba98
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-05-04 01:04:55 +0530

    Refactor : Code for test max in one generic class

--- src/com/genericsproblem/GenericsMaximum.java
+++ src/com/genericsproblem/GenericsMaximum.java
@@ -1,15 +1,34 @@
 package com.genericsproblem;
 
-public class GenericsMaximum {
+/*
+ * generics class which extend comparable
+ * @param <generics>
+ */
+
+public class GenericsMaximum<generics extends Comparable<generics>> {
+    generics s, t, r;
+
     /*
-     * method for test maximum using generics which extends comparable
+     * constructor of generics maximum
+     * @param s
+     * @param t
+     * @param r
+     */
+    public GenericsMaximum(generics s, generics t, generics r) {
+        this.s = s;
+        this.t = t;
+        this.r = r;
+    }
+
+    /*
+     * generics method for test maximum which extend comparable
      * @param a
      * @param b
      * @param c
-     * @param <generics>
+     * @param <E>
      */
-    public static <generics extends Comparable<generics>> void testMax(generics a, generics b, generics c) {
-        generics maximum = a;
+    public static <E extends Comparable<E>> void testMax(E a, E b, E c) {
+        E maximum = a;
         if (b.compareTo(maximum) > 0) {
             maximum = b;
         }
@@ -17,16 +36,16 @@ public class GenericsMaximum {
             maximum = c;
         }
         System.out.println("Maximum is " + maximum);
+
     }
 
     /*
-     * main method for call the testMax method for integer,float and strings
+     * main method for calling the test maximum method and checking for integer, float and string
      * @param args
      */
-
     public static void main(String[] args) {
-        testMax(10,20,30);
-        testMax(12.3f,23.4f,34.5f);
-        testMax("rahul","kundan","sahil");
+        testMax(10, 20, 30);
+        testMax(12.3f, 23.4f, 34.5f);
+        testMax("rahul", "kumar", "sahi");
     }
 }
